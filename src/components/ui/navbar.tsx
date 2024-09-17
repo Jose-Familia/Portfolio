2d5e266a631e488be99005e8baba6df759afb202
Jose Familia <familiajoserene@gmail.com>
2024-09-17 18:06:50 -0400

    Eliminar el ícono de Twitter del componente Navbar

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -9,23 +9,6 @@ const icons = [
   { href: "/", Icon: Home, label: "Home" },
   { href: "https://github.com/Jose-Familia", Icon: Github, label: "GitHub"},
   { href: "https://www.linkedin.com/in/jrfamilia", Icon: Linkedin, label: "LinkedIn"},
-  { href: "https://twitter.com", Icon: ({ size, ...props }) => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={size}
-      height={size}
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M4 4l11.733 16h4.267l-11.733 -16z" />
-      <path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" />
-    </svg>
-  ), label: "Twitter" },
   { href: "mailto:familiajoserene@gmail.com", Icon: Mail, label: "Contact" },
 ]
 
