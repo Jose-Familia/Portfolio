4b5ec0dcb6b2f84e928d5841eb87450cd1ccf628
Jose Familia <128924389+Jose-Familia@users.noreply.github.com>
2024-10-31 20:40:10 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Jose Familia | Portfolio",
-  description: "Portfolio Page",
-  keywords: "Jose Rene Familia, web developer, frontend developer, portfolio",
-  author: "Jose Rene Familia",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
