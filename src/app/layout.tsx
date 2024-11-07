4749cf270e92be9fb15cc00519767701fb04f6a0
Jose Familia <128924389+Jose-Familia@users.noreply.github.com>
2024-11-07 14:22:07 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,19 +7,6 @@ import '@/styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-// Define metadata de esta forma para que Next.js los procese automáticamente
-export const metadata = {
-  title: 'Jose Familia | Portfolio',
-  description: 'Portfolio personal de Jose Familia. Desarrollador web especializado en React, Next.js y Typescript.',
-  keywords: ['desarrollador web', 'frontend', 'backend', 'React', 'Next.js', 'Node.js'],
-  openGraph: {
-    type: 'website',
-    locale: 'es_ES',
-    url: 'https://www.josefamilia.me',
-    siteName: 'Portfolio de Jose Familia',
-  },
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es" suppressHydrationWarning>
