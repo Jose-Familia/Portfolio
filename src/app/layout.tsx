dd28bb4788c6f94e2c6ab612023fcaf2945a58be
Jose Familia <128924389+Jose-Familia@users.noreply.github.com>
2024-11-07 14:19:50 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,17 @@
-import Head from 'next/head';
+// app/layout.tsx
 import { Inter } from 'next/font/google';
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from '@/components/theme-provider';
 import About from '@/components/about';
 import Footer from '@/components/footer';
-import Projects from '@/components/projects';
-import Skills from '@/components/skillSection';
 import '@/styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
+// Define metadata de esta forma para que Next.js los procese automáticamente
 export const metadata = {
   title: 'Jose Familia | Portfolio',
   description: 'Portfolio personal de Jose Familia. Desarrollador web especializado en React, Next.js y Typescript.',
   keywords: ['desarrollador web', 'frontend', 'backend', 'React', 'Next.js', 'Node.js'],
-  authors: [{ name: 'Jose Familia' }],
   openGraph: {
     type: 'website',
     locale: 'es_ES',
@@ -25,15 +23,6 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es" suppressHydrationWarning>
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta name="keywords" content={metadata.keywords.join(', ')} />
-        <meta property="og:type" content={metadata.openGraph.type} />
-        <meta property="og:locale" content={metadata.openGraph.locale} />
-        <meta property="og:url" content={metadata.openGraph.url} />
-        <meta property="og:site_name" content={metadata.openGraph.siteName} />
-      </Head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
